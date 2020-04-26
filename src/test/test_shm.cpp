67905496325b4ed11669aa437736fbeb3832d66e
alephzero <48891271+alephzero@users.noreply.github.com>
2020-04-26 14:20:34 -0700

    bug in test (#29)

--- src/test/test_shm.cpp
+++ src/test/test_shm.cpp
@@ -58,10 +58,11 @@ TEST_CASE_FIXTURE(ShmTestFixture, "shm] bad size") {
       .size = std::numeric_limits<off_t>::max(),
       .resize = false,
   };
-  REQUIRE(a0_shm_open("/foo/bar", &shmopt, &shm) == EINVAL);
+  errno_t err = a0_shm_open("/foo", &shmopt, &shm);
+  REQUIRE((err == ENOMEM || err == EINVAL));
 
   shmopt.size = -1;
-  REQUIRE(a0_shm_open("/foo/bar", &shmopt, &shm) == EINVAL);
+  REQUIRE(a0_shm_open("/bar", &shmopt, &shm) == EINVAL);
 }
 
 TEST_CASE_FIXTURE(ShmTestFixture, "shm] bad path") {
