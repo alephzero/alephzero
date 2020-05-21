436e1e560b0a5fc0626b4b5523088ba4b77cb500
Leonid Shamis <lshamis@thirdwave.ai>
2020-05-21 13:35:06 -0700

    GitHub CI

---
+++ .github/workflows/ci_local.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd "$(dirname "$0")"/../..
+
+rm /tmp/alephzero_ci.Dockerfile
+cat <<EOF >> /tmp/alephzero_ci.Dockerfile
+FROM alpine:latest
+
+RUN apk add --no-cache bash curl
+
+RUN curl https://raw.githubusercontent.com/nektos/act/master/install.sh | bash
+EOF
+
+docker build -t alephzero_ci . -f /tmp/alephzero_ci.Dockerfile
+
+docker run \
+  --rm -it \
+  -v ${PWD}:/alephzero \
+  -w /alephzero \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker \
+  alephzero_ci act $@
