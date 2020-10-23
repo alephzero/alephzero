a91dabccd515bcf7d300119ec9a9273c8b01d0ac
Leonid Shamis <lshamis@bob.localdomain>
2020-10-22 22:26:10 -0700

    fix format

--- src/file.c
+++ src/file.c
@@ -8,9 +8,9 @@
 #include <fcntl.h>
 #include <ftw.h>
 #include <libgen.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
