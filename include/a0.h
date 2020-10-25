8f16202defc20953dfc9117072f4d17795b9b01e
Leonid Shamis <lshamis@bob.localdomain>
2020-10-25 11:24:56 -0700

    Fix a0.h

--- include/a0.h
+++ include/a0.h
@@ -2,9 +2,11 @@
 #define A0_H
 
 #include <a0/alloc.h>
+#include <a0/arena.h>
 #include <a0/common.h>
-#include <a0/file_arena.h>
+#include <a0/errno.h>
 #include <a0/heartbeat.h>
+#include <a0/legacy_arena.h>
 #include <a0/logger.h>
 #include <a0/packet.h>
 #include <a0/prpc.h>
@@ -13,6 +15,7 @@
 #include <a0/time.h>
 #include <a0/topic_manager.h>
 #include <a0/transport.h>
+#include <a0/uuid.h>
 
 #ifdef __cplusplus
 #include <a0/alephzero.hpp>
