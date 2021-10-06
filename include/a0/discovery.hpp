f1fb4f5b01df279ed8516ff17fa84e1f024eacba
Leonid Shamis <leonid.shamis@gmail.com>
2021-10-06 13:13:01 -0700

    explicit construct

--- include/a0/discovery.hpp
+++ include/a0/discovery.hpp
@@ -9,7 +9,7 @@ namespace a0 {
 
 struct PathGlob : details::CppWrap<a0_pathglob_t> {
   PathGlob() = default;
-  PathGlob(std::string path_pattern);
+  explicit PathGlob(std::string path_pattern);
 
   bool match(const std::string& path) const;
 };
