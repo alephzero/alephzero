6a2c56395d4e56d6c169d8ae5d981cb99e0f9f8b
alephzero <48891271+alephzero@users.noreply.github.com>
2021-10-19 16:52:44 -0700

    verbatim discovery (#89)

--- src/test/test_discovery.cpp
+++ src/test/test_discovery.cpp
@@ -60,6 +60,22 @@ TEST_CASE("discovery] pathglob match") {
   bool match;
   a0_pathglob_t glob;
 
+  REQUIRE_OK(a0_pathglob_init(&glob, "/dev/shm/a/foo.a0"));
+
+  REQUIRE_OK(a0_pathglob_match(&glob, "/dev/shm/a/foo.a0", &match));
+  REQUIRE(match);
+
+  REQUIRE_OK(a0_pathglob_match(&glob, "/dev/shm/a/b/foo.a0", &match));
+  REQUIRE(!match);
+
+  REQUIRE_OK(a0_pathglob_init(&glob, "/dev/shm/*/foo.a0"));
+
+  REQUIRE_OK(a0_pathglob_match(&glob, "/dev/shm/a/foo.a0", &match));
+  REQUIRE(match);
+
+  REQUIRE_OK(a0_pathglob_match(&glob, "/dev/shm/a/b/foo.a0", &match));
+  REQUIRE(!match);
+
   REQUIRE_OK(a0_pathglob_init(&glob, "/dev/shm/*/*.a0"));
 
   REQUIRE_OK(a0_pathglob_match(&glob, "/dev/shm/a/foo.a0", &match));
@@ -102,6 +118,14 @@ TEST_CASE("discovery] pathglob match") {
 
 TEST_CASE("discovery] cpp pathglob match") {
   a0::PathGlob glob;
+
+  glob = a0::PathGlob("/dev/shm/a/foo.a0");
+  REQUIRE(glob.match("/dev/shm/a/foo.a0"));
+  REQUIRE(!glob.match("/dev/shm/a/b/foo.a0"));
+
+  glob = a0::PathGlob("/dev/shm/*/foo.a0");
+  REQUIRE(glob.match("/dev/shm/a/foo.a0"));
+  REQUIRE(!glob.match("/dev/shm/a/b/foo.a0"));
 
   glob = a0::PathGlob("/dev/shm/*/*.a0");
   REQUIRE(glob.match("/dev/shm/a/foo.a0"));
@@ -233,7 +257,7 @@ TEST_CASE("discovery] cpp discovery") {
     data.cv.wait(lock, [&data] { return data.paths.size() >= 13; });
   }
 
-  discovery = a0::Discovery();
+  discovery = {};
 
   std::sort(data.paths.begin(), data.paths.end());
   REQUIRE(data.paths == std::vector<std::string>{
@@ -250,5 +274,65 @@ TEST_CASE("discovery] cpp discovery") {
                             "/dev/shm/discovery_test/a/b/file.a0",
                             "/dev/shm/discovery_test/a/file.a0",
                             "/dev/shm/discovery_test/file.a0",
+                        });
+}
+
+TEST_CASE("discovery] cpp discovery exact match") {
+  try {
+    a0::File::remove_all("/dev/shm/discovery_test/");
+  } catch (...) {
+  }
+  a0::File("/dev/shm/discovery_test/unused");
+  a0::File::remove("/dev/shm/discovery_test/unused");
+
+  struct data_t {
+    std::vector<std::string> paths;
+    std::condition_variable cv;
+    std::mutex mu;
+  } data;
+
+  a0::File("/dev/shm/discovery_test/file.a0");
+  a0::File("/dev/shm/discovery_test/a/file.a0");
+  a0::File("/dev/shm/discovery_test/a/b/file.a0");
+
+  a0::Discovery discovery_before(
+      "/dev/shm/discovery_test/a/file.a0",
+      [&](const std::string& path) {
+        std::unique_lock<std::mutex> lock(data.mu);
+        data.paths.push_back(path);
+        data.cv.notify_one();
+      });
+
+  a0::Discovery discovery_after(
+      "/dev/shm/discovery_test/a/b/c/d/file1.a0",
+      [&](const std::string& path) {
+        std::unique_lock<std::mutex> lock(data.mu);
+        data.paths.push_back(path);
+        data.cv.notify_one();
+      });
+
+  a0::File("/dev/shm/discovery_test/a/b/c/d/file1.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/file2.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file1.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file2.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file3.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file4.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file5.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file6.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file7.a0");
+  a0::File("/dev/shm/discovery_test/a/b/c/d/e/f/g/h/i/j/k/l/m/file8.a0");
+
+  {
+    std::unique_lock<std::mutex> lock(data.mu);
+    data.cv.wait(lock, [&data] { return data.paths.size() >= 2; });
+  }
+
+  discovery_before = {};
+  discovery_after = {};
+
+  std::sort(data.paths.begin(), data.paths.end());
+  REQUIRE(data.paths == std::vector<std::string>{
+                            "/dev/shm/discovery_test/a/b/c/d/file1.a0",
+                            "/dev/shm/discovery_test/a/file.a0",
                         });
 }
