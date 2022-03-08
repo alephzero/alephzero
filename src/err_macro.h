9c3a022daa66561756a9fcc8d2926a88f8410995
alephzero <48891271+alephzero@users.noreply.github.com>
2022-03-07 21:01:27 -0800

    assert.h conflicts with libc (#124)

--- src/err_macro.h
+++ src/err_macro.h
@@ -3,11 +3,17 @@
 
 #include <a0/err.h>
 #include <a0/inline.h>
+#include <a0/unused.h>
 
+#include <assert.h>
 #include <errno.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdio.h>
+
+#ifdef DEBUG
+#include <stdlib.h>
+#endif
 
 A0_STATIC_INLINE
 a0_err_t A0_MAKE_SYSERR(int syserr) {
@@ -48,5 +54,49 @@ a0_err_t A0_MAKE_MSGERR(const char* fmt, ...) {
       return _err;              \
     }                           \
   } while (0)
+
+#define A0_ASSERT(X, MSG, ...) \
+  do {                         \
+    A0_MAYBE_UNUSED(X);        \
+  } while (0)
+
+#define A0_ASSERT_OK(ERR, MSG, ...) \
+  do {                              \
+    A0_MAYBE_UNUSED(ERR);           \
+  } while (0)
+
+#ifdef DEBUG
+
+#undef A0_ASSERT
+#define A0_ASSERT(X, MSG, ...)                           \
+  do {                                                   \
+    if (!(X)) {                                          \
+      fprintf(stderr, "AlephZero Assertion Failed!\n");  \
+      fprintf(stderr, "File: %s\n", __FILE__);           \
+      fprintf(stderr, "Line: %d\n", __LINE__);           \
+      fprintf(stderr, "Func: %s\n", __func__);           \
+      fprintf(stderr, "Expr: %s\n", #X);                 \
+      fprintf(stderr, "Msg:  " MSG "\n", ##__VA_ARGS__); \
+      abort();                                           \
+    }                                                    \
+  } while (0)
+
+#undef A0_ASSERT_OK
+#define A0_ASSERT_OK(ERR, MSG, ...)                                \
+  do {                                                             \
+    a0_err_t _err = (ERR);                                         \
+    if (_err) {                                                    \
+      fprintf(stderr, "AlephZero Assertion Failed!\n");            \
+      fprintf(stderr, "File: %s\n", __FILE__);                     \
+      fprintf(stderr, "Line: %d\n", __LINE__);                     \
+      fprintf(stderr, "Func: %s\n", __func__);                     \
+      fprintf(stderr, "Expr: %s\n", #ERR);                         \
+      fprintf(stderr, "Err:  [%d] %s\n", _err, a0_strerror(_err)); \
+      fprintf(stderr, "Msg:  " MSG "\n", ##__VA_ARGS__);           \
+      abort();                                                     \
+    }                                                              \
+  } while (0)
+
+#endif  // DEBUG
 
 #endif  // A0_SRC_ERR_MACRO_H
